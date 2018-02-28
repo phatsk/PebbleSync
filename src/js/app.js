845d4e69c1d83ca70b94ea4f378fae76b0fe5a73
Zach Owen <phatsk@gmail.com>
2018-02-27 23:34:24 -0500

    Images are back?

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,7 @@ var Vector2 = require('vector2');
 
 var main = new UI.Card({
   title: 'Pebble.js',
+  icon: 'images/menu_icon.png',
   subtitle: 'Hello World!',
   body: 'Press any button.',
   subtitleColor: 'indigo', // Named colors
@@ -22,6 +23,7 @@ main.on('click', 'up', function(e) {
     sections: [{
       items: [{
         title: 'Pebble.js',
+        icon: 'images/menu_icon.png',
         subtitle: 'Can do Menus'
       }, {
         title: 'Second Item',
